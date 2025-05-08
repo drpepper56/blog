9971cc707f46a290ae2767914171371bec71500f
Karol Bura <virtualreader76@gmail.com>
2025-05-08 16:11:53 +0100

    heroku deploy testing in production hhihihihihihi

--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,11 @@ export default defineConfig({
   },
   define: {
     'process.env.NODE_ENV': '"production"' // Inject environment variables
+  },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   }
 })
